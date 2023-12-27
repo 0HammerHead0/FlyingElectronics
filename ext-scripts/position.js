8a124e0324674818acd2e96ab86a9cf7872955e4
0HammerHead0 <shashwatakahammer@gmail.com>
2023-12-27 08:00:11 +0530

    ignore model

---
+++ ext-scripts/position.js
@@ -0,0 +1,26 @@
+export function setupPosition(element) {
+    // Get the elements
+    const webglElement = document.querySelector('.webgl');
+    const blobElement = document.getElementById('blob');
+    const blurElement = document.getElementById('blur');
+
+    // Get the center coordinates of the webgl element
+    const webglRect = webglElement.getBoundingClientRect();
+    const webglCenterX = webglRect.left + webglRect.width / 2;
+    const webglCenterY = webglRect.top + webglRect.height / 2;
+
+    // Calculate blob element's position based on the webgl center
+    const blobWidth = blobElement.offsetWidth;
+    const blobHeight = blobElement.offsetHeight;
+    const blurWidth =  webglRect.width;
+    const blurHeight = webglRect.height;
+    // Set the position of blob element to align centers
+    blobElement.style.position = 'absolute';
+    blobElement.style.left = `${webglCenterX - blobWidth / 2}px`;
+    blobElement.style.top = `${webglCenterY - blobHeight / 2}px`;
+    blurElement.style.position = 'absolute';
+    blurElement.style.left = `${webglCenterX - blurWidth / 2}px`;
+    blurElement.style.top = `${webglCenterY - blurHeight / 2}px`;
+    blurElement.style.width = `${blurWidth}px`;
+    blurElement.style.height = `${blurHeight}px`;
+}
--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import './style.css'
-import { setupCounter } from './counter.js'
+import { setupPosition } from './ext-scripts/position.js'
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';

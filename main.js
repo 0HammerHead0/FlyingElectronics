505ef844d11ba15186e827a0a11debeeb5e76dd5
0HammerHead0 <shashwatakahammer@gmail.com>
2023-12-27 17:38:14 +0530

    deploy-temp

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ import * as THREE from 'three';
 import  {gui} from './ext-scripts/gui.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js';
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js';
+import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass.js';
+import { FXAAShader } from 'three/examples/jsm/shaders/FXAAShader.js';
 import gsap from 'gsap';
 
 const container = document.querySelector('.webgl-content');
@@ -19,13 +23,11 @@ const OrbitControl = new OrbitControls(camera, canvas);
 OrbitControl.enableDamping = true;
 OrbitControl.minDistance = 0.45;
 const renderer = new THREE.WebGLRenderer({ antialias: true, canvas:canvas , alpha: true });
+renderer.shadowMap.enabled = true;
 renderer.setSize(sizes.width, sizes.height);
 const axesHelper = new THREE.AxesHelper();
 camera.position.set(0 , 0, 0.7);
 scene.add(axesHelper);
-
-
-
 
 var model;
 const gltfLoader = new GLTFLoader();
@@ -35,9 +37,16 @@ gltfLoader.load(
         model = gltf.scene;
         model.rotateY(Math.PI);
         model.scale.set(10,10,10);
+        model.traverse((child) => {
+          if (child.isMesh) {
+              child.castShadow = true;
+              child.receiveShadow = true;
+          }
+        });
         scene.add(model);
         leftLight.target = model;
         rightLight.target = model;
+        frontLight.target = model;
     },
     (xhr) => {
         const progress = (xhr.loaded / xhr.total).toFixed(2);
@@ -48,15 +57,45 @@ gltfLoader.load(
         console.log('error', error);
     }
 );
-
-const leftLight = new THREE.DirectionalLight(0xffffff, 30);
+const shadowRes = 512;
+const  near = 0.5;
+const far = 500;
+const leftLight = new THREE.DirectionalLight(0xffffff, 10);
 leftLight.position.set(-20, 20, 20);
-const rightLight = new THREE.DirectionalLight(0xffffff, 30);
+leftLight.castShadow = true;
+leftLight.shadow.mapSize.width = shadowRes;
+leftLight.shadow.mapSize.height = shadowRes;
+leftLight.shadow.camera.near = near;
+leftLight.shadow.camera.far = far;
+
+const rightLight = new THREE.DirectionalLight(0xffffff, 8);
 rightLight.position.set(20, 20, -10);
-scene.add(rightLight,leftLight);
+rightLight.castShadow = true;
+rightLight.shadow.mapSize.width = shadowRes;
+rightLight.shadow.mapSize.height = shadowRes;
+rightLight.shadow.camera.near = near;
+rightLight.shadow.camera.far = far;
 
-gui(rightLight, leftLight);
+const frontLight = new THREE.DirectionalLight(0xffffff, 10);
+frontLight.position.set(10,2,10);
+frontLight.castShadow = true;
+frontLight.shadow.mapSize.width = shadowRes;
+frontLight.shadow.mapSize.height = shadowRes;
+frontLight.shadow.camera.near = near;
+frontLight.shadow.camera.far = far;
+scene.add(rightLight,leftLight,frontLight);
 
+
+gui(rightLight, leftLight,frontLight);
+
+// post processign
+const composer = new EffectComposer(renderer);
+const renderPass = new RenderPass(scene, camera);
+composer.addPass(renderPass);
+
+const fxaaPass = new ShaderPass(FXAAShader);
+fxaaPass.uniforms['resolution'].value.set(1 / sizes.width, 1 / sizes.height);
+composer.addPass(fxaaPass);
 
 
 setupPosition();
@@ -65,7 +104,8 @@ function tick(){
     const currentTime = Date.now();
     time = currentTime;
     OrbitControl.update();
-    requestAnimationFrame(tick);
     renderer.render(scene, camera);
+    // composer.render();
+    requestAnimationFrame(tick);
 }
 tick();
